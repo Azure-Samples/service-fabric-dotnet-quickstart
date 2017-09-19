7fb5ac560ccbca0b7adf919127e712b4cc8976d2
Ubuntu <vturecek@vturecek-linux-dev.erxqnvmgmutu3nhphtum4ygpod.dx.internal.cloudapp.net>
2017-09-19 05:45:59 +0000

    Updating Linux deploy script.

--- src/Linux/deploy.sh
+++ src/Linux/deploy.sh
@@ -2,7 +2,7 @@
 DIR=`dirname $0`
 
 sfctl cluster select --endpoint http://localhost:19080
-sfctl application upload --path Voting/pkg/VotingAppPkg
-sfctl application provision --application-type-build-path VotingAppPkg
-sfctl application create --app-name fabric:/Voting --app-type VotingNetCoreType --app-version 1.0
+sfctl application upload --path Voting/pkg/VotingAppPkg --show-progress -verbose
+sfctl application provision --application-type-build-path VotingAppPkg --debug
+sfctl application create --app-name fabric:/Voting --app-type VotingNetCoreType --app-version 1.0.0 --debug
 
