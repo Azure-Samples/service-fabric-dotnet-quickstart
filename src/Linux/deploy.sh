10268940fcc93b1ec81ad7f59761d7dac725f261
Hrushikesh <hrushib@microsoft.com>
2017-09-20 23:53:43 +0530

    Fixed deploy script

--- src/Linux/deploy.sh
+++ src/Linux/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 DIR=`dirname $0`
 
-sfctl cluster select --endpoint http://localhost:19080
-sfctl application upload --path Voting/pkg/VotingAppPkg --show-progress -verbose
+sfctl application upload --path Voting/pkg/VotingAppPkg --show-progress
 sfctl application provision --application-type-build-path VotingAppPkg --debug
 sfctl application create --app-name fabric:/Voting --app-type VotingNetCoreType --app-version 1.0.0 --debug
 
