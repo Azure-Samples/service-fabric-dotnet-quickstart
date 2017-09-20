d63b2e08990de483378a555169d7e391b4be5b74
Hrushikesh <hrushib@microsoft.com>
2017-09-20 23:38:29 +0530

    Fixing root folder location.

---
+++ VotingWeb/ViewExpander.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace VotingWeb
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+
+        /// <summary>
+        /// Used to specify the locations that the view engine should search to 
+        /// locate views.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="viewLocations"></param>
+        /// <returns></returns>
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            string rootDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string viewFullPath = Path.Combine(rootDir, "Views/{2}/{1}/{0}.cshtml");
+            //{2} is area, {1} is controller,{0} is the action
+            string[] locations = new string[] { viewFullPath };
+            return locations.Union(viewLocations);          //Add mvc default locations after ours
+        }
+
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values["customviewlocation"] = nameof(ViewLocationExpander);
+        }
+    }
+}
--- src/Linux/VotingWeb/Startup.cs
+++ src/Linux/VotingWeb/Startup.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -14,10 +17,14 @@ namespace VotingWeb
     {
         public Startup(IHostingEnvironment env)
         {
+            string rootDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string appsettingsFileFullPath = Path.Combine(rootDir, "appsettings.json");
+            string appsettingsEnvFileFullPath = Path.Combine(rootDir, $"appsettings.{env.EnvironmentName}.json");
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile(appsettingsFileFullPath, optional: false, reloadOnChange: true)
+                .AddJsonFile(appsettingsEnvFileFullPath, optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
@@ -29,6 +36,10 @@ namespace VotingWeb
         {
             // Add framework services.
             services.AddMvc();
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new ViewLocationExpander());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
