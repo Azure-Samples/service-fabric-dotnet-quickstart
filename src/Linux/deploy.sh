8f61cde7865a39cf4e5e91bd27f68306f6face0e
Ubuntu <vturecek@vturecek-linux-dev-south.zsanofqj42texhyyjz33hj3uwe.jx.internal.cloudapp.net>
2017-09-24 03:45:28 +0000

    Adding bat files for Windows deployments, updating replica count.

--- src/Linux/deploy.sh
+++ src/Linux/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=`dirname $0`
 
-sfctl application upload --path Voting/pkg/VotingAppPkg --verbose
+sfctl application upload --path Voting/pkg/VotingAppPkg --debug
 sfctl application provision --application-type-build-path VotingAppPkg --debug
 sfctl application create --app-name fabric:/Voting --app-type VotingNetCoreType --app-version 1.0.0 --debug
 
